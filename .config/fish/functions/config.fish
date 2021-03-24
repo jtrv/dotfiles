7a05ac8cef37bad32da2476ab22df75cae025bdc
Jacob Travers <jtravers@tutanota.com>
2021-03-24 12:09:43 -0700

    removing fish files other than fish_plugins

--- .config/fish/functions/config.fish
+++
@@ -1,4 +0,0 @@
-# Defined via `source`
-function config --wraps='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME' --description 'alias config=/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
-  /usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME $argv; 
-end
