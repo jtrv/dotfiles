5bd0da41d4f77a29a85c92bec7610247722d8f49
Jacob Travers <jtravers@tutanota.com>
2021-03-28 00:19:14 -0700

    ADD: redshift, autostartx, broot conf

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -2,3 +2,10 @@
 function config --wraps='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME' --description 'alias config=/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
   /usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME $argv; 
 end
+
+# Start X at login
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
+end
