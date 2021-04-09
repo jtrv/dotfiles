ba42d4d4e160528c87a32c7e9c3730907ca05157
Jacob Travers <jtravers@tutanota.com>
2021-04-09 13:01:50 -0700

    clean cargo output, add fish plugins

--- .config/fish/functions/warehouse.fish
+++ .config/fish/functions/warehouse.fish
@@ -1,10 +1,16 @@
 # Defined via `source`
 function warehouse
-    echo -e "--yay\n"    		> ~/.warehouse &&
-    yay -Qetq               	>> ~/.warehouse &&
-    echo -e "\n\n\n--cargo\n" 	>> ~/.warehouse &&
-    cargo install --list    	>> .warehouse &&
+  echo -e "--yay\n"    		              > ~/.warehouse &&\
+  yay -Qetq               	            >> ~/.warehouse &&\
+  echo -e "\n\n\n--cargo\n" 	          >> ~/.warehouse &&\
+  cargo install --list | sd "v\d.*\n" "\n" |\
+  sd "^\s.*\n" "" 											>> ~/.warehouse &&\
+  echo -e "\n\n\n--fisher\n" 	          >> ~/.warehouse &&\
+  /bin/cat ~/.config/fish/fish_plugins  >> ~/.warehouse &&\
+  echo -e "\n\n\n--Oh-My-Fish\n"        >> ~/.warehouse &&\
+  /bin/ls ~/.config/fish/conf.d/        >> ~/.warehouse &&\
 
-    config add ~/.warehouse &&
-    config commit -m "warehousing new packages"
+  config diff ~/.warehouse &&
+  config add ~/.warehouse &&
+  config commit -m "Warehousing packages"
 end
