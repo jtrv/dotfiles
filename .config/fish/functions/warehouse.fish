93964a148ffb52f378b1620e513c750ffa49c1ba
Jacob Travers <jtravers@tutanota.com>
2021-04-26 13:35:43 -0700

    REFORMAT/ADD: make pretty, add paru instead of yay

--- .config/fish/functions/warehouse.fish
+++ .config/fish/functions/warehouse.fish
@@ -1,16 +1,21 @@
 # Defined via `source`
 function warehouse
-  echo -e "--yay\n"    		              > ~/.warehouse &&\
-  yay -Qetq               	            >> ~/.warehouse &&\
-  echo -e "\n\n\n--cargo\n" 	          >> ~/.warehouse &&\
-  cargo install --list | sd "v\d.*\n" "\n" |\
-  sd "^\s.*\n" "" 											>> ~/.warehouse &&\
-  echo -e "\n\n\n--fisher\n" 	          >> ~/.warehouse &&\
-  /bin/cat ~/.config/fish/fish_plugins  >> ~/.warehouse &&\
-  echo -e "\n\n\n--Oh-My-Fish\n"        >> ~/.warehouse &&\
-  /bin/ls ~/.config/fish/conf.d/        >> ~/.warehouse &&\
 
-  config diff ~/.warehouse &&
-  config add ~/.warehouse &&
-  config commit -m "Warehousing packages"
+    echo -e "--paru\n"                     > ~/.warehouse
+    paru -Qetq                            >> ~/.warehouse
+
+    echo -e "\n\n\n--cargo\n"             >> ~/.warehouse
+        cargo install --list |\
+        sd "v\d.*\n" "\n" |\
+        sd "^\s.*\n" ""                   >> ~/.warehouse
+      
+    echo -e "\n\n\n--fisher\n" 	          >> ~/.warehouse
+    /bin/cat ~/.config/fish/fish_plugins    >> ~/.warehouse
+
+    echo -e "\n\n\n--Oh-My-Fish\n"          >> ~/.warehouse
+    /bin/ls ~/.config/fish/conf.d/          >> ~/.warehouse
+
+    config diff ~/.warehouse &&
+    config add ~/.warehouse &&
+    config commit -m "Warehousing packages"
 end
