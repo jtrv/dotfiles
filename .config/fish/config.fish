eb35f7f07278ed8425951f99771601924edb640a
Jacob Travers <jtravers@tutanota.com>
2021-04-26 15:56:06 -0700

    ADD: vi bindings, fix indentation

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 # Defined via `source`
 
 ### EXPORT ###
+fish_vi_key_bindings
 
 set fish_greeting           # Supresses fish's intro message
 set EDITOR "kak -c sesh"    # kakoune in default sesh
@@ -18,82 +19,82 @@ set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
 # Functions needed for !! and !$
 function __history_previous_command
-  switch (commandline -t)
-  case "!"
-    commandline -t $history[1]; commandline -f repaint
-  case "*"
-    commandline -i !
-  end
+    switch (commandline -t)
+    case "!"
+        commandline -t $history[1]; commandline -f repaint
+    case "*"
+        commandline -i !
+    end
 end
 
 function __history_previous_command_arguments
-  switch (commandline -t)
-  case "!"
-    commandline -t ""
-    commandline -f history-token-search-backward
-  case "*"
-    commandline -i '$'
-  end
+    switch (commandline -t)
+    case "!"
+        commandline -t ""
+        commandline -f history-token-search-backward
+    case "*"
+        commandline -i '$'
+    end
 end
 
 # The bindings for !! and !$
 if [ $fish_key_bindings = fish_vi_key_bindings ];
-  bind -Minsert ! __history_previous_command
-  bind -Minsert '$' __history_previous_command_arguments
+    bind -Minsert ! __history_previous_command
+    bind -Minsert '$' __history_previous_command_arguments
 else
-  bind ! __history_previous_command
-  bind '$' __history_previous_command_arguments
+    bind ! __history_previous_command
+    bind '$' __history_previous_command_arguments
 end
 
-# Function for creating a backup file
+    # Function for creating a backup file
 # ex: backup file.txt
 # result: copies file as file.txt.bak
 function backup --argument filename
-  cp $filename $filename.bak
+    cp $filename $filename.bak
 end
 
 # Function for copying files and directories, even recursively.
 # ex: copy DIRNAME LOCATIONS
 # result: copies the directory and all of its contents.
 function copy
-  set count (count $argv | tr -d \n)
-  if test "$count" = 2; and test -d "$argv[1]"
-  	set from (echo $argv[1] | trim-right /)
-  	set to (echo $argv[2])
-    command cp -r $from $to
-  else
-    command cp $argv
-  end
+    set count (count $argv | tr -d \n)
+        if test "$count" = 2; and test -d "$argv[1]"
+            set from (echo $argv[1] | trim-right /)
+            set to (echo $argv[2])
+        command cp -r $from $to
+    else
+        command cp $argv
+    end
 end
 
 # Function for printing a column (splits input on whitespace)
 # ex: echo 1 2 3 | coln 3
 # output: 3
 function coln
-  while read -l input
+    while read -l input
     echo $input | awk '{print $'$argv[1]'}'
-  end
+    end
 end
 
 # Function for printing a row
 # ex: seq 3 | rown 3
 # output: 3
 function rown --argument index
-  sed -n "$index p"
+    sed -n "$index p"
 end
 
 # Function for ignoring the first 'n' lines
 # ex: seq 10 | skip 5
 # results: prints everything but the first 5 lines
 function skip --argument n
-  tail +(math 1 + $n)
+    tail +(math 1 + $n)
 end
 
 # Function for taking the first 'n' lines
 # ex: seq 10 | take 5
 # results: prints only the first 5 lines
 function take --argument number
-  head -$number
+    head -$number
 end
 
 ### END OF FUNCTIONS ###
