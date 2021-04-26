03608dcf2950c07a8fc62319f9290c114c5592e7
Jacob Travers <jtravers@tutanota.com>
2021-04-26 15:25:33 -0700

    FIX: clean paru output of xorg,xdg,xf86 pkgs

--- .config/fish/functions/warehouse.fish
+++ .config/fish/functions/warehouse.fish
@@ -1,21 +1,21 @@
 # Defined via `source`
 function warehouse
 
-    echo -e "--paru\n"                     > ~/.warehouse
-    paru -Qetq                            >> ~/.warehouse
-
-    echo -e "\n\n\n--cargo\n"             >> ~/.warehouse
+    echo -e "--paru\n"                   > ~/.warehouse
+        paru -Qetq | sd "^x[d-o].*\n" "" >> ~/.warehouse
+        
+    echo -e "\n\n\n--cargo\n"            >> ~/.warehouse
         cargo install --list |\
         sd "v\d.*\n" "\n" |\
-        sd "^\s.*\n" ""                   >> ~/.warehouse
+        sd "^\s.*\n" ""                  >> ~/.warehouse
       
-    echo -e "\n\n\n--fisher\n" 	          >> ~/.warehouse
-    /bin/cat ~/.config/fish/fish_plugins    >> ~/.warehouse
+    echo -e "\n\n\n--fisher\n" 	         >> ~/.warehouse
+    /bin/cat ~/.config/fish/fish_plugins >> ~/.warehouse
 
-    echo -e "\n\n\n--Oh-My-Fish\n"          >> ~/.warehouse
-    /bin/ls ~/.config/fish/conf.d/          >> ~/.warehouse
+    echo -e "\n\n\n--Oh-My-Fish\n"       >> ~/.warehouse
+    /bin/ls ~/.config/fish/conf.d/       >> ~/.warehouse
 
-    config diff ~/.warehouse &&
+    config diff ~/.warehouse # &&
     config add ~/.warehouse &&
     config commit -m "Warehousing packages"
 end
