e660164ebd4f8010d731e9e3488758d9fe1afed5
Jacob Travers <jtravers@tutanota.com>
2021-04-26 15:29:06 -0700

    REMOVE: .fish files for aliases in config

--- .config/fish/functions/config.fish
+++
@@ -1,4 +0,0 @@
-# Defined via `source`
-function config --wraps='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME' --description 'alias for gitting dotfiles'
-  /usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME $argv; 
-end
--- .config/fish/functions/lazyconf.fish
+++
@@ -1,4 +0,0 @@
-# Defined via `source`
-function lazyconf --wraps='lazygit --git-dir=$HOME/dotfiles/ --work-tree=$HOME' --description 'alias for lazygitting dotfiles'
-  lazygit --git-dir=$HOME/dotfiles/ --work-tree=$HOME $argv; 
-end
