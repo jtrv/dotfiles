b4eade1e33d98fc6f55a9ff76ed1918d4575c735
Jacob Travers <jtravers@tutanota.com>
2021-05-16 21:16:54 -0700

    FIX: configure ls commands to preference

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -132,11 +132,11 @@ alias j='joshuto'
 alias bs='broot --sizes'
 
 # Changing "ls" to "exa"
-alias ls='exa -al --color=always --group-directories-first' # my preferred listing
-alias la='exa -a --color=always --group-directories-first'  # all files and dirs
-alias ll='exa -l --color=always --group-directories-first'  # long format
-alias lt='exa -aT --color=always --group-directories-first' # tree listing
-alias l.='exa -a | egrep "^\."'
+alias ls='exa -l --color=always --group-directories-first --git --icons' # my preferred listing
+alias la='exa -al --color=always --group-directories-first --git --icons'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first --git --icons'  # long format
+alias lt='exa -aT --color=always --group-directories-first --git --icons' # tree listing
+alias lh='exa -al --color=always --group-directories-first --git --icons --ignore-glob="[a-z]*|[A-Z]*|[0-9]*"'
 
 # changing "cat" to "bat"
 alias cat='bat'
