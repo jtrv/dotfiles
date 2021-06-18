a81b46e79132166e28fc69d754f6109cea226113
Jacob Travers <travershasemail@gmail.com>
2021-06-18 13:46:07 -0700

    FIX: move .warehouse to config

--- .config/fish/functions/warehouse.fish
+++ .config/fish/functions/warehouse.fish
@@ -1,18 +1,18 @@
 # Defined via `source`
 function warehouse
 
-    echo -e "--paru\n"                   > ~/.warehouse
-        paru -Qetq | sd "^x[d-o].*\n" "" >> ~/.warehouse
+    echo -e "--paru\n"                   > ~/.config/.warehouse
+        paru -Qetq | sd "^x[d-o].*\n" "" >> ~/.config/.warehouse
         
-    echo -e "\n\n\n--cargo\n"            >> ~/.warehouse
+    echo -e "\n\n\n--cargo\n"            >> ~/.config/.warehouse
         cargo install --list |\
         sd "v\d.*\n" "\n" |\
-        sd "^\s.*\n" ""                  >> ~/.warehouse
+        sd "^\s.*\n" ""                  >> ~/.config/.warehouse
       
-    echo -e "\n\n\n--fish-plugins\n" 	 >> ~/.warehouse
-    /bin/cat ~/.config/fish/fish_plugins >> ~/.warehouse
+    echo -e "\n\n\n--fish-plugins\n" 	 >> ~/.config/.warehouse
+    /bin/cat ~/.config/fish/fish_plugins >> ~/.config/.warehouse
 
-    config diff ~/.warehouse
-    config add ~/.warehouse &&
+    config diff ~/.config/.warehouse
+    config add ~/.config/.warehouse &&
     config commit -m "Warehousing packages"
 end
