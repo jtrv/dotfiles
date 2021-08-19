36374112c646d288e2e01ac287eed0b538573ce4
Jacob Travers <travershasemail@gmail.com>
2021-08-19 08:59:39 -0700

    FIX: use first match, (mouse input)

--- .config/fish/functions/trackball-speed.fish
+++ .config/fish/functions/trackball-speed.fish
@@ -1,7 +1,7 @@
 function trackball-speed
 set -l trackBallId (\
     xinput list |\
-    rg '.*Logitech MX Ergo.*id=(\d\d).*' -r '${1}'
+    rg -m 1 '.*Logitech MX Ergo.*id=(\d\d).*' -r '${1}'
   )
 xinput --set-prop $trackBallId 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 .3
 end
