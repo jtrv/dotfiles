dad60a15079a4e84aa37a6ba2b8eddeecbdae4c3
Jacob Travers <travershasemail@gmail.com>
2021-08-25 10:23:01 -0700

    FIX: trackball pointer id regex

--- .config/fish/functions/trackball-speed.fish
+++ .config/fish/functions/trackball-speed.fish
@@ -1,7 +1,7 @@
 function trackball-speed
 set -l trackBallId (\
-    xinput list |\
-    rg -m 1 '.*Logitech MX Ergo.*id=(\d\d).*' -r '${1}'
+xinput list |\
+    rg -m 1 '^.*?Logitech MX Ergo.*?id=(\d+).*?pointer.*$' -r '${1}'
   )
 xinput --set-prop $trackBallId 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 .3
 end
