e9f3967c9aeed658850672e71b7104320becadb3
JacobTravers <travershasemail@gmail.com>
2021-10-26 11:42:06 -0700

    DROP: xcape from megazord script

--- .config/fish/functions/megazord.fish
+++ .config/fish/functions/megazord.fish
@@ -1,8 +1,7 @@
-# this script is for frankensteining my laptop to my desktop monitor and peripherals
+# this script is for frankensteining my laptop settings to my desktop monitor and peripherals
 function megazord
   setxkbmap us -option ctrl:nocaps &&	# *holding* capslock = ctrl
-  xcape -e 'Control_L=Escape' &&      # *tapping* capslock = escape
-  trackball-speed &&                  # function for sensitivity
-  bass mons -s &&                     # display to second monitor only
-  feh --randomize --bg-scale --no-fehbg ~/pictures/wallpapers/
+  trackball-speed &&                  # set thumbball sensitivity
+  bass mons -s &&                     # display on second monitor only
+  feh --randomize --bg-scale --no-fehbg ~/pictures/wallpapers/ # adjust wallpaper for monitor
 end
