49639ed58f5ff7bf87e45b9da4ce10951f6d62de
JacobTravers <travershasemail@gmail.com>
2021-11-03 14:12:39 -0700

    FIX: remove whitespace error

--- .config/fish/functions/warehouse.fish
+++ .config/fish/functions/warehouse.fish
@@ -3,12 +3,12 @@ function warehouse
 
     echo -e "--paru\n"                   > ~/.config/.warehouse
         paru -Qetq | sd "^x[d|f|o].*\n" "" >> ~/.config/.warehouse
-        
+
     echo -e "\n\n\n--cargo\n"            >> ~/.config/.warehouse
         cargo install --list |\
-        sd "v\d.*\n" "\n" |\
+        sd "\sv\d.*\n" "\n" |\
         sd "^\s.*\n" ""                  >> ~/.config/.warehouse
-      
+
     echo -e "\n\n\n--fish-plugins\n" 	 >> ~/.config/.warehouse
     /bin/cat ~/.config/fish/fish_plugins >> ~/.config/.warehouse
 
