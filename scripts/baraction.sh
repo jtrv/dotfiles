d6bed1661f27aeafd014173483edbc36659dc7bd
JacobTravers <travershasemail@gmail.com>
2021-11-08 10:22:34 -0800

    FIX: network greps

--- scripts/baraction.sh
+++ scripts/baraction.sh
@@ -47,9 +47,9 @@ mic() {
 ##############################
 #    NETWORK
 ##############################
-network() {
+net() {
   wire="$(mullvad status | rg Connected | wc -l)"
-  wifi="$(ip a | grep wlp61s0 | grep UP | wc -l)"
+  wifi="$(ip a | grep 'wlp4s0|enp6s0' | grep 'state UP' | wc -l)"
 
   if [ $wire = 1 ]; then
     echo ""
@@ -62,7 +62,7 @@ network() {
 ##############################
 #    BLUETOOTH
 ##############################
-bluetooth() {
+blu() {
   bluetoothStatus="$(bluetoothctl info | rg 'Connected: yes' | wc -l)"
 
   if [ $bluetoothStatus = 1 ]; then
@@ -74,53 +74,39 @@ bluetooth() {
 ##############################
 #    BATTERY
 ##############################
-bat() {
-  batstat="$(cat /sys/class/power_supply/BAT0/status)"
-  percent="$(cat /sys/class/power_supply/BAT0/capacity)"
-  if [ $batstat = 'Unknown' ]; then
-    batstat=""
-  elif [ $batstat = "Charging" ]; then
-    batstat=""
-  elif [[ $percent -ge 5 ]] && [[ $percent -le 19 ]]; then
-    batstat=""
-  elif [[ $percent -ge 20 ]] && [[ $percent -le 39 ]]; then
-    batstat=""
-  elif [[ $percent -ge 40 ]] && [[ $percent -le 59 ]]; then
-    batstat=""
-  elif [[ $percent -ge 60 ]] && [[ $percent -le 79 ]]; then
-    batstat=""
-  elif [[ $percent -ge 80 ]] && [[ $percent -le 95 ]]; then
-    batstat=""
-  elif [[ $percent -ge 96 ]] && [[ $percent -le 100 ]]; then
-    batstat=""
-  fi
+# bat() {
+#   batstat="$(cat /sys/class/power_supply/BAT0/status)"
+#   percent="$(cat /sys/class/power_supply/BAT0/capacity)"
+#   if [ $batstat = 'Unknown' ]; then
+#     batstat=""
+#   elif [ $batstat = "Charging" ]; then
+#     batstat=""
+#   elif [[ $percent -ge 5 ]] && [[ $percent -le 19 ]]; then
+#     batstat=""
+#   elif [[ $percent -ge 20 ]] && [[ $percent -le 39 ]]; then
+#     batstat=""
+#   elif [[ $percent -ge 40 ]] && [[ $percent -le 59 ]]; then
+#     batstat=""
+#   elif [[ $percent -ge 60 ]] && [[ $percent -le 79 ]]; then
+#     batstat=""
+#   elif [[ $percent -ge 80 ]] && [[ $percent -le 95 ]]; then
+#     batstat=""
+#   elif [[ $percent -ge 96 ]] && [[ $percent -le 100 ]]; then
+#     batstat=""
+#   fi
 
-  battery=$(printf %3d $percent)
+#   battery=$(printf %3d $percent)
 
-  echo "$batstat$battery%"
-}
+#   echo "$batstat$battery%"
+# }
 ##############################
 #    VOLUME
 ##############################
-# vol() {
-#   percent="$(amixer get Master | awk -F'[][]' 'END{ print $2 }')"
-#   vol=$(printf %3d $percent)
-#   echo -e "$vol%"
-# }
-##############################
-#    WEATHER
-##############################
-# weather() {
-#   wthr="$(sed 20q ~/.config/weather.txt | grep value | awk '{print $2 $3}' | sed 's/"//g')"
-#   echo " $wthr"
-# }
-##############################
-#    TEMP
-##############################
-# temp() {
-#   tmp="$(grep temp_F ~/.config/weather.txt | awk '{print $2}' | sed 's/"//g' | sed 's/,/ F/g')"
-#   echo " $tmp"
-# }
+vol() {
+  percent="$(amixer get Master | awk -F'[][]' 'END{ print $2 }')"
+  vol=$(printf %3d $percent)
+  echo -e "$vol%"
+}
 ##############################
 #    PACKAGES
 ##############################
@@ -141,6 +127,6 @@ bat() {
 SLEEP_SEC=2
 #loops forever outputting a line every SLEEP_SEC secs
 while :; do
-  echo "$(cpu) $(ram) $(ssd) | $(mic) $(network) $(bluetooth) | $(bat) |"
+  echo "$(cpu)   $(ram)   $(ssd)   | $(vol) | $(mic) $(net) $(blu) |"
   sleep $sleep_sec
 done
