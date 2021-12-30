0d6a4d752b983bee3135cda86f0d277ca1d8bd5f
JacobTravers <travershasemail@gmail.com>
2021-12-29 22:51:50 -0800

    FIX: pactl changes, (pipewire?)

--- .config/fish/functions/tv-mode.fish
+++ .config/fish/functions/tv-mode.fish
@@ -1,5 +1,5 @@
 # this script is for frankensteining my laptop to my desktop monitor and peripherals
 function tv-mode
   mons -m &
-  pactl set-card-profile 0 output:hdmi-stereo-extra1
+  pactl set-card-profile 41 output:hdmi-stereo-extra1
 end
