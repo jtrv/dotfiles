69f8338a04f7c064cd96a0c9126833eea4f6ca96
JacobTravers <travershasemail@gmail.com>
2022-01-20 16:14:35 -0800

    FIX: use pnpm for warehousing instead

--- .config/fish/functions/warehouse.fish
+++ .config/fish/functions/warehouse.fish
@@ -3,6 +3,6 @@ function warehouse
   paru -Qqet > ~/.config/warehouse/pacman
   cargo install --list | rg -o '^(\w.+)\s' -r '$1' > ~/.config/warehouse/cargo
   /bin/cat ~/.config/fish/fish_plugins > ~/.config/warehouse/fish
-  npm list -g | rg -o '(\w.+)@' -r '$1' > ~/.config/warehouse/npm
+  pnpm list -g | rg -o '(\w.+)\s\d' -r '$1' > ~/.config/warehouse/npm
   config diff ~/.config/warehouse/*
 end
