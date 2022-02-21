10670ffc7ec89f3a11c92ec7050111fc9c202ae1
JacobTravers <travershasemail@gmail.com>
2022-02-21 14:07:17 -0800

    ADD: tabtab for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,9 @@ starship init fish | source
 # smarter 'cd' (z)
 zoxide init fish | source
 
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
 
 
 ######## FUNCTIONS ########
