5cb14ec01d84c048970dfb5bb3638ee5ba859c0e
JacobTravers <travershasemail@gmail.com>
2022-03-15 09:23:42 -0700

    ADD: konf -> kakrc, kish -> fish.config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -87,27 +87,28 @@ end
 
 ######## ALIASES ########
 
-alias ani   'ani-cli -q high' # watch anime in super ultra HD
-alias bc    'kalk'
-alias cat   'bat'
-alias conf  '/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME' # git for config files
-alias doas  'doas --'
-alias j     'lf'
-alias la    'exa -al --color=always --group-directories-first --git --icons' # all files and dirs
-alias lc    'lazygit --git-dir=$HOME/.cfg/ --work-tree=$HOME' # lazygit for config files
-alias lg    'lazygit'
-alias lh    'exa -al --color=always --group-directories-first --git --icons --ignore-glob="[a-z]*|[A-Z]*|[0-9]*"' # hidden
-alias ll    'exa -l --color=always --group-directories-first --git --icons'  # long format
-alias ls    'exa -l --color=always --group-directories-first --git --icons' # my preferred listing
-alias lt    'exa -aT --color=always --group-directories-first --git --icons' # tree listing
-alias mirr  'doas reflector --save /etc/pacman.d/mirrorlist --protocol https --country US --latest 200 --sort age' # update mirrorlist
-alias mons  'bass mons' # use bash for mons (monitor scripts)
-alias newbg 'feh --randomize --bg-scale --no-fehbg ~/pictures/wallpapers/' # change bg
-alias np    'pnpm'
-alias npx   'pnpm dlx'
-alias off   'systemctl suspend' # save state, enter low-power mode
-alias pom   'potato' # shell pomodoro timer
-alias q     'exit'
+alias ani    'ani-cli -q high' # watch anime in super ultra HD 8k lossless greenray
+alias bc     'kalk'
+alias cat    'bat'
+alias config '/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME' # git for config files
+alias doas   'doas --'
+alias j      'lf'
+alias kish   'k ~/.config/fish/config.fish'
+alias konf   'k ~/.config/kak/kakrc'
+alias la     'exa -al --color=always --group-directories-first --git --icons' # all files and dirs
+alias lc     'lazygit --git-dir=$HOME/.cfg/ --work-tree=$HOME' # lazygit for config files
+alias lg     'lazygit'
+alias lh     'exa -al --color=always --group-directories-first --git --icons --ignore-glob="[a-z]*|[A-Z]*|[0-9]*"' # hidden
+alias ll     'exa -l --color=always --group-directories-first --git --icons'  # long format
+alias ls     'exa -l --color=always --group-directories-first --git --icons' # preferred listing
+alias lt     'exa -aT --color=always --group-directories-first --git --icons' # tree listing
+alias mons   'bass mons' # use bash for mons (monitor scripts)
+alias newbg  'feh --randomize --bg-scale --no-fehbg ~/pictures/wallpapers/' # change bg
+alias np     'pnpm'
+alias npx    'pnpm dlx'
+alias off    'systemctl suspend' # save state, enter low-power mode
+alias pom    'potato' # shell pomodoro timer
+alias q      'exit'
 
 
 ######## KAKOUNE ########
