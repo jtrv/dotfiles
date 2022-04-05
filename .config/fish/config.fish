e6288343771325cb8993b1814d6a490929e3ccdb
JacobTravers <travershasemail@gmail.com>
2022-04-05 16:25:20 -0700

    DROP: unnecessary copy function

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -54,20 +54,6 @@ function backup --argument filename
   cp $filename $filename.bak
 end
 
-# Function for copying files and directories, even recursively.
-#   ex: copy DIRNAME LOCATIONS
-#   result: copies the directory and all of its contents.
-function copy
-  set count (count $argv | tr -d \n)
-    if test "$count" = 2; and test -d "$argv[1]"
-      set from (echo $argv[1] | trim-right /)
-      set to (echo $argv[2])
-    command cp -r $from $to
-    else
-    command cp $argv
-    end
-end
-
 function how
   kak -e "kakpipe -n how -- hors -a -n 2 -p never $argv"
 end
