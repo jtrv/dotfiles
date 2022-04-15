f50cf5445242dc9746d5469f0f1ccc2098fd53d4
JacobTravers <travershasemail@gmail.com>
2022-04-14 20:44:09 -0700

    FIX: baraction

--- scripts/baraction.sh
+++ scripts/baraction.sh
@@ -3,15 +3,20 @@
 ##############################
 #    CPU
 ##############################
-cpu() {
-  percent="$(\
-  awk '{u=$2+$4; t=$2+$4+$5; \
-  if (NR==1){u1=u; t1=t;} \
-  else print ($2+$4-u1) * 100 / (t-t1) "%"; }' \
-  <(grep 'cpu ' /proc/stat) <(sleep 1;grep 'cpu ' /proc/stat))"
-  cpu=$(printf %3d $percent)
-  echo -e "$cpu%"
-}
+# TODO work on this bs
+# cpu() {
+#   percent="$(
+#     awk '{u=$2+$4; t=$2+$4+$5; \
+#   if (NR==1){u1=u; t1=t;} \
+#   else print ($2+$4-u1) * 100 / (t-t1) "%"; }' \
+#       <(grep 'cpu ' /proc/stat) <(
+#         sleep 1
+#         grep 'cpu ' /proc/stat
+#       )
+#   )"
+#   cpu=$(printf %3d "$percent")
+#   echo "$cpu%"
+# }
 ##############################
 #    RAM
 ##############################
@@ -19,23 +24,23 @@ ram() {
   used="$(free | grep Mem: | awk '{print $3}')"
   total="$(free | grep Mem: | awk '{print $2}')"
 
-  percent=$(( 200 * $used/$total - 100 * $used/$total ))
+  percent=$((200 * used / total - 100 * used / total))
   ram=$(printf %3d $percent)
 
-  echo -e "$ram%"
+  echo "$ram%"
 }
 ##############################
 #    STORAGE
 ##############################
 ssd() {
   SSD="$(df -h /home | grep /dev | awk '{print $5}')"
-    echo -e " $SSD"
+  echo " $SSD"
 }
 ##############################
 #    MIC
 ##############################
 mic() {
-  state=`amixer -D pulse get Capture toggle | gawk 'match($0, /(Front Left|Mono).*\[(.*)\]/, a) {print a[2]}'`
+  state=$(amixer -D pulse get Capture toggle | gawk 'match($0, /(Front Left|Mono).*\[(.*)\]/, a) {print a[2]}')
 
   if [ "$state" = "on" ]; then
     echo ""
@@ -48,11 +53,11 @@ mic() {
 ##############################
 net() {
   wire="$(mullvad status | rg Connected | wc -l)"
-  wifi="$(ip a | grep wlp61s0 | grep UP | wc -l)"
+  wifi="$(ip a | grep wlp61s0 | grep -c UP)"
 
-  if [ $wire = 1 ]; then
+  if [ "$wire" = "1" ]; then
     echo ""
-  elif [ $wifi = 1 ]; then
+  elif [ "$wifi" = "1" ]; then
     echo ""
   else
     echo "睊"
@@ -64,7 +69,7 @@ net() {
 blu() {
   bluetoothStatus="$(bluetoothctl info | rg 'Connected: yes' | wc -l)"
 
-  if [ $bluetoothStatus = 1 ]; then
+  if [ "$bluetoothStatus" = "1" ]; then
     echo ""
   else
     echo ""
@@ -76,25 +81,25 @@ blu() {
 bat() {
   batstat="$(cat /sys/class/power_supply/BAT0/status)"
   percent="$(cat /sys/class/power_supply/BAT0/capacity)"
-  if [ $batstat = 'Unknown' ]; then
+  if [ "$batstat" = "Unknown" ]; then
     batstat=""
-  elif [ $batstat = "Charging" ]; then
+  elif [ "$batstat" = "Charging" ]; then
     batstat=""
-  elif [[ $percent -ge 5 ]] && [[ $percent -le 19 ]]; then
+  elif [ "$percent" -ge 5 ] && [ "$percent" -le 19 ]; then
     batstat=""
-  elif [[ $percent -ge 20 ]] && [[ $percent -le 39 ]]; then
+  elif [ "$percent" -ge 20 ] && [ "$percent" -le 39 ]; then
     batstat=""
-  elif [[ $percent -ge 40 ]] && [[ $percent -le 59 ]]; then
+  elif [ "$percent" -ge 40 ] && [ "$percent" -le 59 ]; then
     batstat=""
-  elif [[ $percent -ge 60 ]] && [[ $percent -le 79 ]]; then
+  elif [ "$percent" -ge 60 ] && [ "$percent" -le 79 ]; then
     batstat=""
-  elif [[ $percent -ge 80 ]] && [[ $percent -le 95 ]]; then
+  elif [ "$percent" -ge 80 ] && [ "$percent" -le 95 ]; then
     batstat=""
-  elif [[ $percent -ge 96 ]] && [[ $percent -le 100 ]]; then
+  elif [ "$percent" -ge 96 ] && [ "$percent" -le 100 ]; then
     batstat=""
   fi
 
-  battery=$(printf %3d $percent)
+  battery=$(printf %3d "$percent")
 
   echo "$batstat$battery%"
 }
@@ -103,22 +108,22 @@ bat() {
 ##############################
 vol() {
   percent="$(amixer get Master | awk -F'[][]' 'END{ print $2 }')"
-  vol=$(printf %3d $percent)
-  echo -e "$vol%"
+  vol=$(printf %3d "$percent")
+  echo "$vol%"
 }
 ##############################
 #    PACKAGES
 ##############################
 # pkgs() {
 #   pkgs="$(pacman -Q | wc -l)"
-#   echo -e "$pkgs"
+#   echo "$pkgs"
 # }
 ##############################
 #    UPGRADES
 ##############################
 # upgrades() {
 #   upgrades="$(pacman -Qu | wc -l)"
-#   echo -e "$upgrades"
+#   echo "$upgrades"
 # }
 ##############################
 #    BAR RENDER
@@ -126,6 +131,6 @@ vol() {
 SLEEP_SEC=2
 #loops forever outputting a line every SLEEP_SEC secs
 while :; do
-  echo "$(cpu)  $(ram)  $(ssd)  | $(vol) | $(mic) $(net) $(blu) | $(bat)"
-  sleep $sleep_sec
+  echo "$(ram)  $(ssd)  | $(vol) | $(mic) $(net) $(blu) | $(bat)"
+  sleep $SLEEP_SEC
 done
