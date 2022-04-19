6a2dddfe754300592a08cd4d94ec0f07f396aa83
JacobTravers <travershasemail@gmail.com>
2022-04-19 10:09:58 -0700

    FORMAT: shift script around

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-wego -b openweathermap -owm-api-key 1ee6beb63c28e624253ee1d53559a079 -l "oxnard" -d 7 -u imperial
+wego -b openweathermap -owm-api-key 1ee6beb63c28e624253ee1d53559a079 -l "oxnard" -u imperial -d 7
