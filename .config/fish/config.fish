14027d6c40b1f8e3978ec898803ad2996423825b
JacobTravers <travershasemail@gmail.com>
2022-04-27 12:14:07 -0700

    ADD: fish cursor stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,11 @@
 fish_vi_key_bindings
 
+# Emulates vim's cursor shape behavior
+set fish_cursor_default block blink
+set fish_cursor_insert line blink
+set fish_cursor_replace_one underscore blink
+set fish_cursor_visual block blink
+
 ######## SOURCE ########
 
 # better history
