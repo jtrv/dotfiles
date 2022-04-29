2c8008982262cb9fc8017c9bc513b17751aa022f
JacobTravers <travershasemail@gmail.com>
2022-04-29 16:56:59 -0700

    FIX: use .local/bin for scripts

--- scripts/rclone-init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-rclone mount mxsftp: ~/mxs/ftp --daemon &
-rclone mount mxsdrive: ~/mxs/drive --daemon --fast-list &
--- scripts/weather.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-wego -b openweathermap -owm-api-key 1ee6beb63c28e624253ee1d53559a079 -l "oxnard" -u imperial -d 7
