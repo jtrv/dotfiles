88935e11b69bfd7073a8dda36e79b443fa20d2bf
JacobTravers <travershasemail@gmail.com>
2022-05-27 08:12:14 -0700

    fix(kak-grep): use grep inside kak

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -139,6 +139,13 @@ function kdn
   kak -e "kakpipe -n mdn -- mdn $argv"
 end
 
+function kak-grep
+  set grepargs
+  for x in $argv
+    set -a grepargs (echo $x | sed -e "s/'/''/g" -e "s/^/'/" -e "s/\$/'/")
+  end
+  kak -e "grep $(string join -- " " $grepargs)"
+end
 complete -c kak-grep -w rg
 alias kg 'kak-grep'
 
