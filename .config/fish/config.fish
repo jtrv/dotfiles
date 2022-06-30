ab80b1ce1051acc83eeca6aa123247924d96e742
JacobTravers <travershasemail@gmail.com>
2022-06-29 20:41:27 -0700

    fix(fish): merged duplicate lines

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -365,7 +365,3 @@ ex=:\
 *.pdf=:\
 *.nix=:\
 "
-
-# tabtab source for packages
-# uninstall by removing these lines
-[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
