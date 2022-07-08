eb2d0c46c18575ff7406e30630269bc6f75c0f50
JacobTravers <travershasemail@gmail.com>
2022-07-08 07:51:05 -0700

    feat(fish): user fisher plugins for starship/zoxide

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,12 +11,6 @@ set fish_cursor_visual block blink
 # better history
 mcfly init fish | source
 
-# better prompt
-starship init fish | source
-
-# better 'cd' (z)
-zoxide init fish | source
-
 # tabtab source for packages
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
