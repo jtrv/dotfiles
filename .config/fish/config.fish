4d482ab13fb0d04c27ba8487f97ae343606b9196
JacobTravers <travershasemail@gmail.com>
2022-10-14 12:55:28 -0700

    fix(fish): drop unnecessary mons alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -85,7 +85,6 @@ alias loc     'plocate'
 alias m       'math'
 alias mirrors '~/.local/bin/update-mirrors' # update mirrors / database
 alias mkdir   'mkdir -p'
-alias mons    'bass mons' # use bash for mons (monitor scripts)
 alias nbg     'feh --randomize --bg-scale --no-fehbg ~/media/pictures/wallpapers/' # change bg
 alias off     'systemctl suspend' # save state, enter low-power mode
 alias pom     'potato' # shell pomodoro timer
