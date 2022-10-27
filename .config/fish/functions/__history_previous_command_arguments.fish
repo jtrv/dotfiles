2b000c4207874d7a49ae994a238673eee3200171
JacobTravers <travershasemail@gmail.com>
2022-10-27 11:11:37 -0700

    fix(fish): use EDITOR instead of vi-mode, move fn's

---
+++ .config/fish/functions/__history_previous_command_arguments.fish
@@ -0,0 +1,10 @@
+# add '!$' functionality
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
