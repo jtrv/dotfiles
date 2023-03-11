be9be8072d49d6fe291a26755f1804b8d5f185ea
JacobTravers <travershasemail@gmail.com>
2023-03-11 15:32:21 -0800

    feat(fish): kamp completions

---
+++ .config/fish/completions/kampliment.fish
@@ -0,0 +1,14 @@
+complete -c kamp --no-files
+complete -c kamp -a attach -d 'attach to a session in context'
+complete -c kamp -a cat -d 'print buffer content'
+complete -c kamp -a ctx -d 'show execution context'
+complete -c kamp -a edit -d 'edit a file'
+complete -c kamp -a get -d 'get state from a session in context'
+complete -c kamp -a init -d 'kakoune init'
+complete -c kamp -a kill -d 'kill session'
+complete -c kamp -a list -d 'list sessions'
+complete -c kamp -a send -d 'send command to a session in context'
+complete -c kamp -a version -d 'display version'
+complete -c kamp -l help -d 'display usage information'
+complete -c kamp -s c -l client -d client
+complete -c kamp -s s -l session -d session
