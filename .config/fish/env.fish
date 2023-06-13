8373bcbfc0eb425258ad00f6e4d9c157e80997ca
JacobTravers <travershasemail@gmail.com>
2023-06-13 13:14:37 -0700

    feat(fish): add github token

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -44,6 +44,7 @@ set -gx BAT_PAGER                "/usr/bin/moar -no-linenumbers"
 set -gx EDITOR                   "/usr/bin/kak"
 set -gx fish_greeting
 set -gx FZF_DEFAULT_OPTS         "--ansi --color=dark --multi --tabstop=2 --preview-window border-vertical --bind='alt-a:select-all,alt-d:deselect-all,ctrl-l:preview-down,ctrl-h:preview-up,alt-j:jump'"
+set -gx GITHUB_TOKEN             (secli get github_token)
 set -gx HORS_ENGINE              "google"
 set -gx MANPAGER                 "/usr/bin/bat"
 set -gx MOZ_X11_EGL              1
