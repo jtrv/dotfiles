2df12949757f5159e800f376b1d9d0f6936eb784
jtrv <jtrv@tutanota.com>
2023-09-11 11:22:11 -0700

    drop(fish): acme.sh stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -29,7 +29,6 @@ end
 
 ######## ALIASES ########
 
-alias acme    "acme.sh --home \"$XDG_CONFIG_HOME\"/acme.sh/"
 alias ani     'ani-cli -q best' # weeb out in S-rank ultra-fidelity 8k lossless greenray
 alias boi     'wikiman'
 alias cat     'bat'
