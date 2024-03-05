c5d6b442bbdd6c9e57563aa22b7b8105c7e41152
jtrv <jtrv@tutanota.com>
2024-03-05 15:15:12 -0800

    feat: use kimono-koans/two_percent (skim)

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -79,6 +79,7 @@ ctrl-l:preview-down,\
 ctrl-h:preview-up,\
 alt-j:jump\
 '"
+
 # catppucin theme
 set -gx FZF_DEFAULT_OPTS "\
 $FZF_DEFAULT_OPTS \
@@ -86,6 +87,17 @@ $FZF_DEFAULT_OPTS \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
 "
+
+set -gx SKIM_DEFAULT_OPTIONS "\
+--ansi \
+--multi \
+--tabstop=2 \
+--bind='alt-a:select-all,\
+alt-d:deselect-all,\
+ctrl-l:preview-down,\
+ctrl-h:preview-up,\
+alt-j:jump\
+'"
 
 # catppucin theme
 set -gx SKIM_DEFAULT_OPTIONS "$SKIM_DEFAULT_OPTIONS --color=fg:#cdd6f4,bg:#1e1e2e,matched:#313244,matched_bg:#f2cdcd,current:#cdd6f4,current_bg:#45475a,current_match:#1e1e2e,current_match_bg:#f5e0dc,spinner:#a6e3a1,info:#cba6f7,prompt:#89b4fa,cursor:#f38ba8,selected:#eba0ac,header:#94e2d5,border:#6c7086"
