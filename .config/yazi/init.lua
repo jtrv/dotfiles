05120e6b583cc76b90f59acb62fddf0cac01ce26
jtrv <jtrv@tutanota.com>
2024-05-14 13:05:09 -0700

    feat(yazi): show user/group + links in status

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,3 +1,30 @@
 require("starship"):setup()
 require("session"):setup({ sync_yanked = true })
 require("relative-motions"):setup({ show_numbers = "relative", show_motion = true })
+
+-- show links in status bar
+function Status:name()
+    local h = cx.active.current.hovered
+    return h and ui.Span(" " .. h.name .. (h.link_to and " -> " .. tostring(h.link_to) or "")) or ui.Span("")
+end
+
+-- show user/group in status bar
+function Status:owner()
+  local h = cx.active.current.hovered
+  return h and ya.target_family() == "unix" and ui.Line {
+    ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+    ui.Span(":"),
+    ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+    ui.Span(" "),
+  } or ui.Line {}
+end
+function Status:render(area)
+  self.area = area
+  local left = ui.Line { self:mode(), self:size(), self:name() }
+  local right = ui.Line { self:owner(), self:permissions(), self:percentage(), self:position() }
+  return {
+    ui.Paragraph(area, { left }),
+    ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
+    table.unpack(Progress:render(area, right:width())),
+  }
+end
