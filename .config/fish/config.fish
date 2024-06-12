a29e2a27eb9c6f18e16527f75ed1080ae7d37b2f
jtrv <jtrv@tutanota.com>
2024-06-11 20:25:33 -0700

    fix(fish): drop yarnrc alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -67,7 +67,6 @@ alias q       'exit'
 alias thes    'thesauromatic'
 alias tv      'lobster'
 alias wget    "wget2 --hsts-file=\"$XDG_DATA_HOME/wget-hsts\""
-alias yarn    "yarn --use-yarnrc \"$XDG_CONFIG_HOME\"/yarn/config"
 
 
 ######## KAKOUNE ########
