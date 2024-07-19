835cb3803d31f84b3904ec9b0c4b7ee41975bd9f
jtrv <jtrv@tutanota.com>
2024-07-19 10:01:40 -0700

    format(fish): align '| source'

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,9 +4,9 @@ fish_default_key_bindings
 
 source ~/.config/fish/env.fish
 
-atuin init fish |             source
+atuin init fish             | source
 pay-respects fish --alias f | source
-starship init fish |          source
+starship init fish          | source
 
 ######## KEY-BINDINGS ########
 
