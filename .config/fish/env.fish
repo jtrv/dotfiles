748c279444f2ed2d22b375f5896656bf8b200f2f
jtrv <46337843+jtrv@users.noreply.github.com>
2024-12-24 03:03:12 -0800

    feat: better tiebreak for skim

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -115,6 +115,7 @@ set -gx SKIM_DEFAULT_OPTIONS "\
 --multi \
 --reverse \
 --tabstop=2 \
+--tiebreak=score \
 --bind='\
 alt-a:select-all,\
 alt-d:deselect-all,\
@@ -130,7 +131,23 @@ ctrl-space:jump\
 '"
 
 # catppucin theme
-set -gx SKIM_DEFAULT_OPTIONS "$SKIM_DEFAULT_OPTIONS --color=fg:#cdd6f4,bg:#1e1e2e,matched:#313244,matched_bg:#f2cdcd,current:#cdd6f4,current_bg:#45475a,current_match:#1e1e2e,current_match_bg:#f5e0dc,spinner:#a6e3a1,info:#cba6f7,prompt:#89b4fa,cursor:#f38ba8,selected:#eba0ac,header:#94e2d5,border:#6c7086"
+set -gx SKIM_DEFAULT_OPTIONS "\
+$SKIM_DEFAULT_OPTIONS \
+--color=fg:#cdd6f4,\
+bg:#1e1e2e,\
+matched:#313244,\
+matched_bg:#f2cdcd,\
+current:#cdd6f4,\
+current_bg:#45475a,\
+current_match:#1e1e2e,\
+current_match_bg:#f5e0dc,\
+spinner:#a6e3a1,\
+info:#cba6f7,\
+prompt:#89b4fa,\
+cursor:#f38ba8,\
+selected:#eba0ac,\
+header:#94e2d5,\
+border:#6c7086"
 
 set -gx LF_ICONS "\
 di=:\
