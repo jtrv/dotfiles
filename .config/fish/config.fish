a4887507665015a3ffe46872275535104de1a65a
jtrv <46337843+jtrv@users.noreply.github.com>
2025-02-12 15:32:02 -0800

    fix(fish): bind fzf_vars to <a-c-v>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,6 +14,8 @@ if status is-interactive
 
 ######## KEY-BINDINGS ########
 
+  fzf_configure_bindings --variables=\e\cv
+
   bind ! __history_previous_command             # add '!!' functionality, req ./functions/__history_previous_command.fish
   bind '$' __history_previous_command_arguments # add '!$' functionality, req ./functions/__history_previous_command_arguments.fish
 
