8ac165f0be80dc81a9edd7aa6ba9be89bc8d6078
jtrv <46337843+jtrv@users.noreply.github.com>
2025-03-05 11:00:20 -0800

    feat(ginit): use fish abbr instead

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -28,6 +28,14 @@ if status is-interactive
   abbr --add --global mv     'mv -i'
   abbr --add --global rm     'cnc'
   abbr --add --global gitbit 'git commit --amend --no-edit --date="Sat 01 Jan 2022 16:20:00 PST"'
+  abbr --add --global ginit  "set CWD_NAME (basename '$PWD')
+gh repo create --private '$CWD_NAME'
+git init
+git add .
+git commit -e
+git remote add origin 'https://github.com/jtravers-mxs/$CWD_NAME.git'
+git branch -M main
+git push -u origin main"
 
 
 ######## ALIASES ########
