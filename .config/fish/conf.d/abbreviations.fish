490c29fb9075f6933137289960b0343f082309d8
jtrv <46337843+jtrv@users.noreply.github.com>
2025-03-19 14:31:10 -0700

    fix(fish): update abbr's

--- .config/fish/conf.d/abbreviations.fish
+++ .config/fish/conf.d/abbreviations.fish
@@ -7,14 +7,14 @@ abbr --add --global rm     'cnc'
 abbr --add --global --set-cursor k- 'k ~/%'
 
 abbr --add --global gitbit 'git commit --amend --no-edit --date="Sat 01 Jan 2022 16:20:00 PST"'
-abbr --add --global --set-cursor ginit  "set CWD_NAME (basename '$PWD')
-gh repo create --private '$CWD_NAME'
-git init
-git add .
-git commit -e
-git remote add origin 'https://github.com/jtravers-mxs%/$CWD_NAME.git'
-git branch -M main
-git push -u origin main"
+abbr --add --global --set-cursor ginit  '
+set CWD_N (basename "$PWD"); set TRUNK_N "main";
+gh repo create --private "$CWD_N";
+git init; git add . ; git commit -e;
+git remote add origin "https://github.com/%/$CWD_N.git";
+git branch -M $TRUNK_N; git push -u origin $TRUNK_N';
+
+abbr --add --global --set-cursor webpz 'magick % -resize 2500x2500 -quality 85 .webp'
 
 end
 
