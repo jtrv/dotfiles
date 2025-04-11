23689b1735bacd2988390ec2307fc1b427adb033
jtrv <46337843+jtrv@users.noreply.github.com>
2025-04-11 08:21:43 -0700

    feat: update llm stuff

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -77,6 +77,7 @@ set -gx fish_greeting
 set -gx HORS_ENGINE                 "google"
 set -gx MANPAGER                    "kak -e 'set buffer filetype man'"
 set -gx OPENAI_API_BASE             "https://api.openai.com/v1"
+set -gx OPENROUTER_API_KEY          (secli get openrouter_api_key)
 set -gx PAGER                       "kak"
 set -gx SCCACHE_DIRECT              true
 set -gx QT_STYLE_OVERRIDE           "kvantum"
