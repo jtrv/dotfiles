4f9ac872374f254fe7b3f336be80a7d11009a2cb
jtrv <46337843+jtrv@users.noreply.github.com>
2025-04-24 18:08:32 -0700

    feat(fzf): use preview-scroll

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -102,7 +102,9 @@ ctrl-b:page-up,\
 ctrl-f:page-down,\
 ctrl-u:half-page-up,\
 ctrl-d:half-page-down,\
-ctrl-space:jump\
+ctrl-space:jump,\
+preview-scroll-up:preview-half-page-up,\
+preview-scroll-down:preview-half-page-down\
 '"
 
 # catppucin theme
@@ -130,7 +132,9 @@ ctrl-b:page-up,\
 ctrl-f:page-down,\
 ctrl-u:half-page-up,\
 ctrl-d:half-page-down,\
-ctrl-space:jump\
+ctrl-space:jump,\
+preview-scroll-up:preview-up,\
+preview-scroll-down:preview-down\
 '"
 
 # catppucin theme
