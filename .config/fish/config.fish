95b4e7cfab675bfbbcbb850af47e7f159a7bf084
jtrv <46337843+jtrv@users.noreply.github.com>
2025-11-14 14:11:21 -0800

    doc(fish): login text

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,7 +14,7 @@ end
 # Start X at login
 if status is-login
   if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-    echo "Starting X11. (<C-c> to cancel)"
+    echo "Starting X11. Press <C-c> to cancel"
     sleep 2
     exec startx -- -keeptty &>/dev/null
   end
