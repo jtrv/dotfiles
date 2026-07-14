ee6d2bf23a2ae06d7907c473bd756e07b84b2bc6
jtrv <46337843+jtrv@users.noreply.github.com>
2026-07-14 14:06:46 -0700

    fish: restore LAST30DAYS_MEMORY_DIR from morpheus

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -71,6 +71,7 @@ set -gx DELTA_PAGER                 "kak"
 set -gx EDITOR                      "kak"
 set -gx fish_greeting
 set -gx HORS_ENGINE                 "google"
+set -gx LAST30DAYS_MEMORY_DIR       "$XDG_DOCUMENTS_DIR/Last30Days"
 set -gx MANPAGER                    "kak -e 'set buffer filetype man'"
 set -gx PAGER                       "kak"
 set -gx SCCACHE_DIRECT              true
